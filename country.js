b8be33fafaae7af23389bc4163bd170f9a146d1e
Subhankar Ghosh <goutamg245@gmail.com>
2024-08-01 00:27:50 +0530

    Update country.js

--- country.js
+++ country.js
@@ -57,7 +57,7 @@ fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`)
         .then(([borderCountry]) =>{
           const borderCountryTag = document.createElement('a')
           borderCountryTag.innerText =borderCountry.name.common
-          borderCountryTag.href = `/Rest%20country%20apii/country.html?name=${borderCountry.name.common}`
+          borderCountryTag.href = `/country.html?name=${borderCountry.name.common}`
           borderCountries.append(borderCountryTag)
         })
       ])
